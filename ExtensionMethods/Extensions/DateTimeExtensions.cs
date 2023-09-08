21eced50b5948a0063514a338b3e8db945a8c067
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-09-08 18:57:43 -0300

    Criando extensões de metodos

---
+++ ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace System
+{
+    static class DateTimeExtensions
+    {
+        static public string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duraction = DateTime.Now - thisObj;
+
+            if (duraction.TotalHours <= 24)
+            {
+                return duraction.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duraction.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Time duraction(dd/MM/yyyy hh:mm): ");
+            DateTime dt = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Time duraction: " + dt.ElapsedTime());
+            Console.WriteLine("_____________________________________");
+            Console.Write("Write something: ");
+            string s = Console.ReadLine();
+            Console.Write("How many characters should remain in the message? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Result: " + s.Cut(n));
+        }
+    }
+}
